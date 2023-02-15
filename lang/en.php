25344e86a96ef1d1609ba12c4a7684f8977e2e93
Dmytro Dyvulskyi <eklerk397@gmail.com>
2023-02-15 23:48:28 +0100

    Translations

--- lang/en.php
+++ lang/en.php
@@ -1,21 +1,74 @@
 <?php /** @noinspection ALL @formatter::off */ return [
 //
 //
+    '<b>PHP_SF</b> framework is a powerful and flexible web application framework written in PHP' 
+    => '<b>PHP_SF</b> framework is a powerful and flexible web application framework written in PHP',
+//
+//
+    'API design and architecture' => 'API design and architecture',
+    'API reference' => 'API reference',
     'Access Denied!' => 'Access Denied!',
+    'Actions and methods' => 'Actions and methods',
     'Apr' => 'Apr',
     'April' => 'April',
     'Aug' => 'Aug',
     'August' => 'August',
+    'Authentication and authorization' => 'Authentication and authorization',
 //
 //
+    'Best practices' => 'Best practices',
+    'Best practices and coding standards' => 'Best practices and coding standards',
+    'Best practices for deploying and hosting the application' 
+    => 'Best practices for deploying and hosting the application',
+//
+//
+    'CRUD operations and querying data' => 'CRUD operations and querying data',
+    'Cache drivers and configurations' => 'Cache drivers and configurations',
+    'Cache tagging and invalidation' => 'Cache tagging and invalidation',
+    'Caching' => 'Caching',
+    'Caching strategies and mechanisms' => 'Caching strategies and mechanisms',
+    'Changelog' => 'Changelog',
+    'Checkout Full Log' => 'Checkout Full Log',
+    'Code coverage and reporting' => 'Code coverage and reporting',
+    'Code documentation and commenting' => 'Code documentation and commenting',
+    'Coding conventions and standards' => 'Coding conventions and standards',
+    'Collaborative documentation' => 'Collaborative documentation',
+    'Command arguments and options' => 'Command arguments and options',
+    'Command-line interface (CLI) tools' => 'Command-line interface (CLI) tools',
+    'Common security threats and best practices' => 'Common security threats and best practices',
+    'Configuring the application' => 'Configuring the application',
+    'Controllers' => 'Controllers',
     'Created At' => 'Created At',
+    'Creating a new project' => 'Creating a new project',
+    'Creating and organizing controllers' => 'Creating and organizing controllers',
+    'Creating custom middleware' => 'Creating custom middleware',
+    'Creating forms and form elements' => 'Creating forms and form elements',
+    'Custom validation and validation groups' => 'Custom validation and validation groups',
+    'Customizing the framework\'s behavior and features' => 'Customizing the framework\'s behavior and features',
 //
 //
+    'DI and service containers' => 'DI and service containers',
+    'Debugging tools and techniques' => 'Debugging tools and techniques',
     'Dec' => 'Dec',
     'December' => 'December',
+    'Defining API routes and endpoints' => 'Defining API routes and endpoints',
+    'Defining database entities and relationships' => 'Defining database entities and relationships',
+    'Defining routes and URL patterns' => 'Defining routes and URL patterns',
+    'Dependency injection and inversion of control' => 'Dependency injection and inversion of control',
+    'Dependency injection in controllers' => 'Dependency injection in controllers',
+    'Deployment and hosting' => 'Deployment and hosting',
+    'Detecting and handling user language preferences' => 'Detecting and handling user language preferences',
 //
 //
     'E-mail' => 'E-mail',
+    'Easy installation' => 'Easy installation',
+    'Encryption and hashing' => 'Encryption and hashing',
+    'Entities and databases' => 'Entities and databases',
+    'Error handling and debugging' => 'Error handling and debugging',
+    'Error reporting and logging' => 'Error reporting and logging',
+    'Event-driven programming and listeners' => 'Event-driven programming and listeners',
+    'Exception handling and custom error pages' => 'Exception handling and custom error pages',
+    'Extensibility and customization' => 'Extensibility and customization',
 //
 //
     'Feb' => 'Feb',
@@ -28,30 +81,124 @@
     'Field `%s` is not a valid email address.' => 'Field `%s` is not a valid email address.',
     'Field `%s` must be one of these numbers: (%s)' => 'Field `%s` must be one of these numbers: (%s)',
     'Field `%s` should be between `%s` and `%s`!' => 'Field `%s` should be between `%s` and `%s`!',
+    'File uploads and handling' => 'File uploads and handling',
+    'Form submission and processing' => 'Form submission and processing',
+    'Forms and validation' => 'Forms and validation',
+    'Functional and integration testing' => 'Functional and integration testing',
+//
+//
+    'Get Started' => 'Get Started',
+    'Get started with the framework and learn how to build your first web app with comprehensive usage examples included in the documentation.' 
+    => 'Get started with the framework and learn how to build your first web app with comprehensive usage examples included in the documentation.',
+    'Global and route-specific middleware' => 'Global and route-specific middleware',
+//
+//
+    'Handling authentication and authorization' => 'Handling authentication and authorization',
+    'Handling file downloads and streaming' => 'Handling file downloads and streaming',
+    'Handling route parameters' => 'Handling route parameters',
+//
+//
+    'Input validation and sanitization' => 'Input validation and sanitization',
+    'Installation' => 'Installation',
+    'Installing dependencies' => 'Installing dependencies',
+    'Internationalization and localization' => 'Internationalization and localization',
+    'Introduction' => 'Introduction',
+//
+//
+    'Key terminology and concepts' => 'Key terminology and concepts',
+//
+//
+    'Language files and translations' => 'Language files and translations',
+    'Layouts and partials' => 'Layouts and partials',
+    'Locale and time zone settings' => 'Locale and time zone settings',
 //
 //
     'Mar' => 'Mar',
     'March' => 'March',
     'May' => 'May',
+    'Middleware chaining and ordering' => 'Middleware chaining and ordering',
+    'Middlewares' => 'Middlewares',
+    'Migrations and schema changes' => 'Migrations and schema changes',
+    'Mocking and faking dependencies' => 'Mocking and faking dependencies',
 //
 //
     'Name' => 'Name',
+    'Named routes and URL generation' => 'Named routes and URL generation',
+    'Naming conventions and file organization' => 'Naming conventions and file organization',
     'Nov' => 'Nov',
     'November' => 'November',
 //
 //
     'Oct' => 'Oct',
     'October' => 'October',
+    'Optimizing performance with caching' => 'Optimizing performance with caching',
+    'Overview of the framework\'s purpose and features' => 'Overview of the framework\'s purpose and features',
 //
 //
+    'PHP_SF offers a rich set of features for easily building robust and scalable web apps and APIs.' 
+    => 'PHP_SF offers a rich set of features for easily building robust and scalable web apps and APIs.',
+    'Passing data to views' => 'Passing data to views',
     'Password' => 'Password',
+    'Performance optimization and refactoring' => 'Performance optimization and refactoring',
+    'Powerful & flexible' => 'Powerful & flexible',
+    'Powerful and flexible web application framework' => 'Powerful and flexible web application framework',
+    'Production vs development environments' => 'Production vs development environments',
 //
 //
+    'Quickly set up your dev environment and start building your app with easy installation and detailed instructions.' 
+    => 'Quickly set up your dev environment and start building your app with easy installation and detailed instructions.',
+//
+//
+    'REST API development' => 'REST API development',
+    'Registering and resolving services' => 'Registering and resolving services',
+    'Request and response formats' => 'Request and response formats',
+    'Request and response handling' => 'Request and response handling',
+    'Route groups and middleware' => 'Route groups and middleware',
+    'Routing' => 'Routing',
+    'Running the application' => 'Running the application',
+//
+//
+    'Security' => 'Security',
     'Sep' => 'Sep',
     'September' => 'September',
+    'Server requirements and configuration' => 'Server requirements and configuration',
+    'Service container and service providers' => 'Service container and service providers',
+    'System requirements and installation' => 'System requirements and installation',
 //
 //
+    'Template inheritance and sections' => 'Template inheritance and sections',
+    'Templating engines and syntax' => 'Templating engines and syntax',
+    'Testing' => 'Testing',
+    'The documentation also covers best practices for building secure, performant, and maintainable web applications.' 
+    => 'The documentation also covers best practices for building secure, performant, and maintainable web applications.',
+    'The documentation provides detailed information about all classes, methods and functions in the API reference.' 
+    => 'The documentation provides detailed information about all classes, methods and functions in the API reference.',
+    'This documentation is a collaborative effort, and welcomes community contributions. If you notice any errors or omissions, please submit a pull request.' 
+    => 'This documentation is a collaborative effort, and welcomes community contributions. If you notice any errors or omissions, please submit a pull request.',
+    'To install the framework simply run the installation script from the project root directory and follow the instructions.' 
+    => 'To install the framework simply run the installation script from the project root directory and follow the instructions.',
+    'Translating dates, currencies, and other formats' => 'Translating dates, currencies, and other formats',
+//
+//
+    'Unit testing and test-driven development' => 'Unit testing and test-driven development',
+    'Uploading and storing files' => 'Uploading and storing files',
+    'Usage examples' => 'Usage examples',
     'User Group' => 'User Group',
+    'User authentication and login' => 'User authentication and login',
+    'User roles and permissions' => 'User roles and permissions',
     'User with this email not found.' => 'User with this email not found.',
+    'Using middleware for authentication, caching, and more' 
+    => 'Using middleware for authentication, caching, and more',
+//
+//
+    'Validating file types and sizes' => 'Validating file types and sizes',
+    'Validation rules and messages' => 'Validation rules and messages',
+    'Views and templates' => 'Views and templates',
+//
+//
+    'Writing and running console commands' => 'Writing and running console commands',
+//
+//
+    'XSS and CSRF protection' => 'XSS and CSRF protection',
 //
 ];
